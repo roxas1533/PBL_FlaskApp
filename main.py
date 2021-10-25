9c1d581aa1ebdc555bb640c92080b2e568bd10c0
roxas1533 <roxas1533@gmail.com>
2021-10-25 18:09:11 +0900

    port確認

--- main.py
+++ main.py
@@ -302,4 +302,5 @@ if __name__ == "__main__":
     subprocess.Popen(
         "{}/goserver/main".format(os.getcwd()), cwd="{}/goserver".format(os.getcwd())
     )
+    print("PORT!:", os.environ.get("PORT"))
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
