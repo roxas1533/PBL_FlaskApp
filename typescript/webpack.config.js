b6898a0c37d6a75987132a1051db2338e94c5e65
roxas1533 <roxas1533@gmail.com>
2021-12-02 19:18:24 +0900

    アップデートノート追記

--- typescript/webpack.config.js
+++ typescript/webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "development",
-  // mode: "production",
+  // mode: "development",
+  mode: "production",
 
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: "./loading.ts",
